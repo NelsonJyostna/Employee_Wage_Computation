ea1bbfdbb1ec680888f4fd3f52a2850e3966eb03
nelson26-shruti-mom <nelsonwasekar555@gmail.com>
2020-06-09 20:42:56 +0530

    Added : Employee Attendance Functionality

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -1,6 +1,13 @@
 public class EmpWageComputation {
 
      public static void main(String[ ] args) {
-        System.out.println("Welcome to Employee Wage Computation Program");
+     // constants
+      int IS_PRESENT = 1 ;
+      // Computation
+     double empcheck = Math.floor(Math.random() * 10 % 2);
+     if (empcheck == IS_PRESENT)
+        System.out.println("Employee is Present");
+     else
+      System.out.println("Employee is Abscent");
      }
 }
