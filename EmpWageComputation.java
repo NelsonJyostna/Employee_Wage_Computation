7a8a96443e9dd096ee5bb87e380278f911346cb0
nelson26-shruti-mom <nelsonwasekar555@gmail.com>
2020-06-10 16:43:21 +0530

    Added : Employee Wage For A Month Using For Loop

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -5,36 +5,42 @@ public class EmpWageComputation {
       int IS_PART_TIME = 1 ;
       int IS_FULL_TIME = 2 ;
       int EMP_RATE_PER_HR = 20 ;
-
+      int TOTAL_SALERY = 0;
+      int NUM_WORKING_DAYS = 20;
+       // Variable
+        int empHrs=0;
       // Computation
-       double empcheck = Math.floor(Math.random( ) * 3);
+       for ( int day=1; day<=NUM_WORKING_DAYS; day++ )
+         {
+        double empcheck = Math.floor(Math.random( ) * 3);
         switch ((int)empcheck)
           {
             case 1:
              {
-              int empHrs = 8;
-                int Salery=empHrs*EMP_RATE_PER_HR;
-                System.out.println("Salery :"+Salery);
+              empHrs = 8;
+                //int Salery=empHrs*EMP_RATE_PER_HR;
+                //System.out.println("Salery :"+Salery);
               break;
              }
            case 2:
              {
-              int empHrs = 4;
-              int Salery=empHrs*EMP_RATE_PER_HR;
-              System.out.println("Salery :"+Salery);
+              empHrs = 4;
+              //int Salery=empHrs*EMP_RATE_PER_HR;
+              //System.out.println("Salery :"+Salery);
              break;
              }
            default:
             {
-             int empHrs = 0;
-             int Salery=empHrs*EMP_RATE_PER_HR;
-             System.out.println("Salery :"+Salery);
+             empHrs = 0;
+             //int Salery=empHrs*EMP_RATE_PER_HR;
+             //System.out.println("Salery :"+Salery);
             break;
             }
           }
-        //int Salery = empHrs * EMP_RATE_PER_HR;
-        //System.out.println("Salery :"+Salery);
+          int Salery = empHrs * EMP_RATE_PER_HR;
+          TOTAL_SALERY = TOTAL_SALERY + Salery;
+          System.out.println("Total Salery :"+TOTAL_SALERY);
+        }
     }
 }
-
 
