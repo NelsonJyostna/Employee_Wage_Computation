21dfb8c4843c1f1919d77dee37b27677dc71a0e7
nelson26-shruti-mom <nelsonwasekar555@gmail.com>
2020-06-10 12:01:20 +0530

    Added : Employee Part Time Hour

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -1,23 +1,33 @@
 public class EmpWageComputation {
 
      public static void main(String[ ] args) {
-     // constants
-      int IS_PRESENT = 1 ;
+      // constants
+      int IS_PART_TIME = 1 ;
+      int IS_FULL_TIME = 2 ;
+      int EMP_RATE_PER_HR = 20 ;
+
       // Computation
-     double empcheck = Math.floor(Math.random() * 10 % 2);
-      if (empcheck == IS_PRESENT)
-      {
-           int empRatePerHr=20;
-           int empHrs=8;
-           int Salery = empHrs*empRatePerHr;
-          System.out.println("Salery : "+Salery);
-      }
+       double empcheck = Math.floor(Math.random( ) * 3);
+      if (IS_PART_TIME == empcheck)
+         {
+            int empHrs = 8;
+            int Salery=empHrs*EMP_RATE_PER_HR;
+            System.out.println("Salery :"+Salery);
+         }
+      else if (IS_FULL_TIME == empcheck)
+         {
+          int empHrs = 4;
+          int Salery=empHrs*EMP_RATE_PER_HR;
+          System.out.println("Salery :"+Salery);
+
+         }
       else
-          {
-          System.out.println("Salery : 0");
-          }
-     }
+         {
+            int empHrs = 0;
+            int Salery=empHrs*EMP_RATE_PER_HR;
+            System.out.println("Salery :"+Salery);
+         }
+       //int Salery=empHrs*EMP_RATE_PER_HR;
+       //System.out.println("Salery :"+Salery);
+    }
 }
-
-
-
