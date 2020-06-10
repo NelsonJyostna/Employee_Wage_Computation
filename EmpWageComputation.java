4f274aa0956c39405d94dad6f3dbced7c1f21805
nelson26-shruti-mom <nelsonwasekar555@gmail.com>
2020-06-10 17:58:31 +0530

    Refracted : Employee Wage With Total Working Hours Or Day Using While Loop

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -1,46 +1,48 @@
 public class EmpWageComputation {
 
      public static void main(String[ ] args) {
-      // constants
-      int IS_PART_TIME = 1 ;
-      int IS_FULL_TIME = 2 ;
-      int EMP_RATE_PER_HR = 20 ;
-      int TOTAL_SALERY = 0;
-      int NUM_WORKING_DAYS = 20;
-       // Variable
+
+      System.out.println("Welcome to Employee Wage Computation Program");
+
+        // constants
+        int IS_PART_TIME = 1 ;
+        int IS_FULL_TIME = 2 ;
+        int EMP_RATE_PER_HR = 20 ;
+        int NUM_WORKING_DAYS = 20;
+        int MAX_HRS_IN_MONTH=100;
+
+        // Variable
         int empHrs=0;
-      // Computation
-       for ( int day=1; day<=NUM_WORKING_DAYS; day++ )
-         {
-        double empcheck = Math.floor(Math.random( ) * 3);
-        switch ((int)empcheck)
+        int totalEmpHrs=0;
+        int totalWorkingDays=0;
+
+        // Computation
+         while ( totalEmpHrs < MAX_HRS_IN_MONTH && totalWorkingDays < NUM_WORKING_DAYS )
           {
-            case 1:
+             totalWorkingDays++;
+             double empcheck = Math.floor(Math.random( ) * 3);
+             switch ((int)empcheck)
              {
-              empHrs = 8;
-                //int Salery=empHrs*EMP_RATE_PER_HR;
-                //System.out.println("Salery :"+Salery);
-              break;
-             }
-           case 2:
-             {
-              empHrs = 4;
-              //int Salery=empHrs*EMP_RATE_PER_HR;
-              //System.out.println("Salery :"+Salery);
-             break;
-             }
-           default:
-            {
-             empHrs = 0;
-             //int Salery=empHrs*EMP_RATE_PER_HR;
-             //System.out.println("Salery :"+Salery);
-            break;
+                 case 1:
+                   {
+                     empHrs = 8;
+                     break;
+                   }
+                 case 2:
+                   {
+                     empHrs = 4;
+                     break;
+                   }
+                default:
+                  {
+                    empHrs = 0;
+                    break;
+                  }
             }
-          }
-          int Salery = empHrs * EMP_RATE_PER_HR;
-          TOTAL_SALERY = TOTAL_SALERY + Salery;
-          System.out.println("Total Salery :"+TOTAL_SALERY);
-        }
+           totalEmpHrs=totalEmpHrs+empHrs;
+         }
+      int totalSalery = totalEmpHrs * EMP_RATE_PER_HR;
+      System.out.println("Total Salery :"+totalSalery);
     }
 }
 
